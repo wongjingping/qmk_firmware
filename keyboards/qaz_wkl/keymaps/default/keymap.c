ccfc866ffa7d485dadee11693c50e45f90b523a6
Jing Ping Wong <jingpingw@twitter.com>
2022-08-13 12:50:32 +0800

    formatting

--- keyboards/qaz_wkl/keymaps/default/keymap.c
+++ keyboards/qaz_wkl/keymaps/default/keymap.c
@@ -40,31 +40,31 @@ combo_t key_combos[COMBO_COUNT] = {COMBO(jk_combo, KC_ESC)};
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     /* Base */
   [_BASE] = LAYOUT(
-    KC_Q,     KC_W,     KC_E,     KC_R,      KC_T,               KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
-    G_A,      KC_S,     KC_D,     KC_F,      KC_G,               KC_H,     KC_J,     KC_K,     KC_L,     G_SC,
-    S_Z,      A_X,      C_C,      KC_V,      KC_B,               KC_N,     C_M,      A_COMM,   S_DOT,
-    A_SLSH,		L3,       L1,        L2,                 KC_RSFT,  KC_RSFT
+    KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,
+    G_A,      KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     G_SC,
+    S_Z,      A_X,      C_C,      KC_V,     KC_B,     KC_N,     C_M,      A_COMM,   S_DOT,
+    A_SLSH,		L3,       L1,       L2,       KC_RSFT,            KC_RSFT
   ),
 
   [_L1] = LAYOUT(
-    KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,            KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_MINS,  KC_EQL,
-    KC_LPRN,  KC_RPRN,  KC_LCBR,  KC_RCBR,  KC_GRV,             KC_QUOT,  KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_PIPE,
-    _______,  KC_SLSH,  KC_QUES,  KC_TAB,   _______,            KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_MPLY,
-    _______,            _______,  _______,  KC_LSFT,            _______,  _______
+    KC_EXLM,  KC_AT,    KC_HASH,  KC_DLR,   KC_PERC,  KC_CIRC,  KC_AMPR,  KC_ASTR,  KC_MINS,  KC_EQL,
+    KC_LPRN,  KC_RPRN,  KC_LCBR,  KC_RCBR,  KC_GRV,   KC_QUOT,  KC_LBRC,  KC_RBRC,  KC_BSLS,  KC_PIPE,
+    _______,  KC_SLSH,  KC_QUES,  KC_TAB,   _______,  KC_MUTE,  KC_VOLD,  KC_VOLU,  KC_MPLY,
+    _______,            _______,  _______,  KC_LSFT,  _______,            _______
   ),
 
   [_L2] = LAYOUT(
-    KC_1,     KC_2,     KC_3,     KC_4,      KC_5,               KC_6,     KC_7,     KC_8,    KC_9,     KC_0,
-    _______,  _______,  _______,  _______,   KC_DOT,             KC_LEFT,  KC_DOWN,  KC_UP,   KC_RIGHT, KC_BSPC,
-    KC_LSFT,  _______,  _______,  _______,   _______,            SG_LBRC,  SG_RBRC,  G_LBRC,   G_RBRC,
-    _______, 		KC_LGUI,  KC_LALT,   _______,            _______,  _______
+    KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,
+    _______,  _______,  _______,  _______,  KC_DOT,   KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_BSPC,
+    KC_LSFT,  _______,  _______,  _______,  _______,  SG_LBRC,  SG_RBRC,  G_LBRC,   G_RBRC,
+    _______, 		KC_LGUI,  KC_LALT,  _______,  _______,            _______
   ),
 
   [_L3] = LAYOUT(
-    KC_F1,    KC_F2,    KC_F3,    KC_F4,     KC_F5,              KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,
-    _______,  _______,  _______,  _______,   _______,            _______,  _______,  _______,  KC_F11,   KC_F12,
-    _______,  _______,  _______,  _______,   _______,            _______,  _______,  _______,  _______,
-    _______, 		_______,  _______,   _______,            _______,  _______
+    KC_F1,    KC_F2,    KC_F3,    KC_F4,     KC_F5,   KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,
+    _______,  _______,  _______,  _______,   _______, _______,  _______,  _______,  KC_F11,   KC_F12,
+    _______,  _______,  _______,  _______,   _______, _______,  _______,  _______,  _______,
+    _______, 		_______,  _______,   _______, _______,            _______
   ),
 };
 
